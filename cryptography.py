2b4ee2aafbb1dde73d6e1a58d43fdc16cc6d29d1
mgardner3901 <31485789+mgardner3901@users.noreply.github.com>
2018-10-11 20:28:16 -0400

    Updated from Brython Server: 10/11/2018 8:28:16 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Morgan Gardner
+Credit: None
 
 Assignment:
 
@@ -10,3 +10,56 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+
+
+end="false"
+while end == "false":
+    stringInt = []
+    keyInt = []
+    encryptedNumbers = []
+    encryptedLetters = ""
+    cyphertextNums = []
+    cypherkeyNums = []
+    decryptedNums = []
+    decryptedLetters = ""
+    userInput = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    
+    if userInput != "e" and userInput != "d" and userInput != "q":
+        print("Did not understand command, try again. ")
+    elif userInput == "q":
+        print("Goodbye!")
+        end = "true"
+    elif userInput == "e":
+        userString = input("Message: ")
+        userKey = input("Key: ")
+        for i in userString:
+            stringInt.append(associations.find(i))
+        for e in userKey:
+            keyInt.append(associations.find(e))
+        while len(keyInt) <= len(stringInt):
+            keyInt = keyInt + keyInt
+        zippedNumbers = zip(stringInt, keyInt)
+        for p in zippedNumbers:
+            encryptedNumbers.append((p[0] + p[1]))
+        for t in encryptedNumbers:
+            if t > 85:
+                t = t - 85
+            encryptedLetters = encryptedLetters + associations[t]
+        print(encryptedLetters) 
+    elif userInput == "d":
+        userCyphertext = input("Message: ")
+        cyphertextKey = input("Key: ")
+        for q in userCyphertext:
+            cyphertextNums.append(associations.find(q))
+        for y in cyphertextKey:
+            cypherkeyNums.append(associations.find(y))
+        while len(cyphertextNums) >= len(cypherkeyNums):
+            cypherkeyNums = cypherkeyNums + cypherkeyNums
+        zippedCypher = zip(cyphertextNums, cypherkeyNums)
+        for r in zippedCypher:
+            decryptedNums.append((r[0] - r[1]))
+        for h in decryptedNums:
+            if h < 0:
+                h = h + 85
+            decryptedLetters = decryptedLetters + associations[h]
+        print(decryptedLetters)
